6b98404deed60a7efdd50a5cfda6d5c379ea1a17
draktorio <draktorios@gmail.com>
2025-11-05 16:18:26 +0300

    Update lena.sql

--- dump/lena.sql
+++ dump/lena.sql
@@ -31,7 +31,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `records` (
   `id` int NOT NULL,
   `name` varchar(255) NOT NULL,
-  `description` varchar(255) NOT NULL,
+  `description` VARCHAR(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
